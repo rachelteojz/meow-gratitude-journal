fe97a7391da5171cd8b9da68cdcf158d92dfd600
rachelteojz <r.teojiazhen@gmail.com>
2026-06-06 00:38:14 +0800

    Add files via upload

---
+++ newsw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'dgl-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request)
+    )
+  );
+});
