3e040a368a166fbbe8fd08999c3d7653c9032bee
rachelteojz <r.teojiazhen@gmail.com>
2026-06-06 00:57:21 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'dgl-v5';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
